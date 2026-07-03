da1e78943785261f9bae473fb3425f905bc4f3e7
fanatique222 <goergedubois690@gmail.com>
2026-07-03 10:58:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,39 +1,24 @@
 import discord
 from discord.ext import commands
+import os
 
 intents = discord.Intents.default()
-intents.members = True  # Obligatoire pour détecter les nouveaux membres
+intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
+async def on_ready():
+    print(f"{bot.user} est en ligne !")
+
+@bot.event
 async def on_member_join(member):
-    # Donne un rôle automatiquement
-    role = discord.utils.get(member.guild.roles, name="Membre")
-    if role:
-        await member.add_roles(role)
+    channel = discord.utils.get(member.guild.text_channels, name="selem")
 
-    # Salon de bienvenue
-    channel = bot.get_channel(1521999167252074627) 
     if channel:
-        embed = discord.Embed(
-            title="🎉 Bienvenue !",
-            description=(
-                f"Bienvenue {member.mention} sur **{member.guild.name}** !\n\n"
-                f"👥 Nous sommes maintenant **{member.guild.member_count} membres**.\n"
-                f"🎭 Le rôle **{membre}** t'a été attribué !"
-            ),
-            color=discord.Color.green()
+        await channel.send(
+            f"👋 Selem {member.mention} sur **{member.guild.name}** !\n"
+            f"👥 Nous sommes maintenant **{member.guild.member_count}** membres."
         )
-
-        if member.avatar:
-            embed.set_thumbnail(url=member.avatar.url)
-
-        await channel.send(embed=embed)
-
-        embed.set_footer(text=f"ID du membre : {member.id}")
-
-        await channel.send(embed=embed)
-import os
 
 bot.run(os.getenv("TOKEN"))
